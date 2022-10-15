1ee8b75211daa530fd10524469c478d802f9f87a
ryanneilparker <ryanneilparker@gmail.com>
2022-10-15 15:07:06 +0000

    Add vehicle.rb

---
+++ easy_1/vehicle.rb
@@ -0,0 +1,39 @@
+class Vehicle
+  attr_reader :make, :model
+
+  def initialize(make, model)
+    @make = make
+    @model = model
+  end
+
+  def wheels
+    4
+  end
+
+  def to_s
+    "#{@make} #{@model}"
+  end
+end
+
+
+class Car < Vehicle
+end
+
+class Motorcycle < Vehicle
+  def wheels
+    2
+  end
+end
+
+class Truck
+  attr_reader :payload
+
+  def initialize(make, model, payload)
+    super(make, model)
+    @payload = payload
+  end
+
+  def wheels
+    6
+  end
+end
